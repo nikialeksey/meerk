fd6d58bc388dc52a35f0548e61a50e724914e4a5
Alexey Nikitin <nikialeksey@gmail.com>
2018-10-02 00:33:43 +0700

    Increase version

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ with open('requirements.txt') as r:
 
 setuptools.setup(
     name='meerk',
-    version='0.2.0',
+    version='0.2.1',
     description='',
     long_description=long_description,
     long_description_content_type="text/markdown",
